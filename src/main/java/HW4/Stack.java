02cc3d49b276f74fc3a33e568dcd1c337fd7e3f0
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-27 11:33:45 +0200

    Создан интерфейс Stack

---
+++ src/main/java/HW4/Stack.java
@@ -0,0 +1,19 @@
+package HW4;
+
+public interface Stack<E> {
+
+    void push(E value);
+
+    E pop();
+
+    E peek(); //top()
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
