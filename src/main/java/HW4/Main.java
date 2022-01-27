430d5c36b186e29a876da22336e69c269b7f56cf
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-27 11:31:27 +0200

    Создан класс SimpleLinkedList

--- src/main/java/HW4/Main.java
+++ src/main/java/HW4/Main.java
@@ -4,4 +4,20 @@ public class Main {
 
 
 
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new SimpleLinkedList<>();
+
+        linkedList.insertFirst(1);
+        linkedList.insertFirst(2);
+        linkedList.insertFirst(3);
+        linkedList.insertFirst(4);
+        linkedList.insertFirst(5);
+        linkedList.insertFirst(6);
+        linkedList.insertFirst(7);
+        linkedList.insertFirst(8);
+
+        linkedList.display();
+
+    }
+
 }
