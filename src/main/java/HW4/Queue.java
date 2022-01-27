0bb7a3dbd69c617386da8b75beee011e31a6dfc3
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-27 11:55:48 +0200

    Создан интерфейс Queue

---
+++ src/main/java/HW4/Queue.java
@@ -0,0 +1,19 @@
+package HW4;
+
+public interface Queue<E> {
+
+    boolean insert(E value);
+
+    E remove();
+
+    E peekFront();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
