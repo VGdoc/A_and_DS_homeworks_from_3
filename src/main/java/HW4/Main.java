b12b2447e403038380a7fe09f534e24cee90ac2a
Vladimir Grigoriev <intvreporting@gmail.com>
2022-01-28 17:05:32 +0200

    Создан и протестирвоан класс TwoSideBidirectionalListImpl (двунаправленный список)

--- src/main/java/HW4/Main.java
+++ src/main/java/HW4/Main.java
@@ -24,6 +24,29 @@ public class Main {
 
         testIterator(linkedList);
 
+        System.out.println("***********************************");
+
+        TwoSideBidirectionalListImpl<Integer> biDirectList = new TwoSideBidirectionalListImpl<>();
+
+        biDirectList.insertFirst(3);
+        biDirectList.insertFirst(2);
+        biDirectList.insertFirst(1);
+
+        biDirectList.insertLast(4);
+        biDirectList.insertLast(5);
+        biDirectList.insertLast(6);
+
+        biDirectList.displayForward();
+        biDirectList.displayBackward();
+
+        biDirectList.remove(3);
+        biDirectList.remove(1);
+        biDirectList.remove(6);
+        biDirectList.remove(10);
+
+        System.out.println();
+        biDirectList.displayForward();
+        biDirectList.displayBackward();
     }
 
     private static void testHomeWork() {
